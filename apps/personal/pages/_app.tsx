58cfb6a2f45215d5d80ebea326836e5fcea501c5
Rendi Rizki <rendirizkir@gmail.com>
2022-07-20 20:30:53 +0700

    fix: scripts

--- apps/personal/pages/_app.tsx
+++ apps/personal/pages/_app.tsx
@@ -1,5 +1,6 @@
 import { ComponentType } from 'react';
 import { NextComponentType } from 'next';
+import Script from 'next/script';
 import type { AppProps } from 'next/app';
 import './styles.scss';
 
@@ -14,11 +15,28 @@ function MyApp({ Component, pageProps }: AppWithLayout) {
   const getLayout: any = Component.Layout || ((page: any) => page);
 
   return (
-    <ThemeProvider attribute="class">
-      <TimelineMenuProvider>
-        {getLayout(<Component {...pageProps} />)}
-      </TimelineMenuProvider>
-    </ThemeProvider>
+    <>
+      <ThemeProvider attribute="class">
+        <TimelineMenuProvider>
+          {getLayout(<Component {...pageProps} />)}
+        </TimelineMenuProvider>
+      </ThemeProvider>
+      <Script
+        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NX_GA_TRACKING_ID}`}
+        strategy="afterInteractive"
+      />
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){window.dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', '${process.env.NX_GA_TRACKING_ID}', { 
+            page_path: window.location.pathname,
+          });
+        `}
+      </Script>
+    </>
   );
 }
 
--- apps/personal/pages/_document.tsx
+++ apps/personal/pages/_document.tsx
@@ -1,6 +1,5 @@
 import { ReactElement } from 'react';
 import Document, { Head, Html, Main, NextScript } from 'next/document';
-import Script from 'next/script';
 
 class MyDocument extends Document {
   render(): ReactElement {
@@ -10,27 +9,6 @@ class MyDocument extends Document {
         <body>
           <Main />
           <NextScript />
-          <Script
-            src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NX_GA_TRACKING_ID}`}
-            strategy="afterInteractive"
-          />
-          <Script id="google-analytics" strategy="afterInteractive">
-            {`
-              window.dataLayer = window.dataLayer || [];
-              function gtag(){window.dataLayer.push(arguments);}
-              gtag('js', new Date());
-
-              gtag('config', '${process.env.NX_GA_TRACKING_ID}', { 
-                page_path: window.location.pathname,
-              });
-            `}
-          </Script>
-          <Script id="heap-analytics" strategy="afterInteractive">
-            {`
-              window.heap=window.heap||[],heap.load=function(e,t){window.heap.appid=e,window.heap.config=t=t||{};var r=document.createElement("script");r.type="text/javascript",r.async=!0,r.src="https://cdn.heapanalytics.com/js/heap-"+e+".js";var a=document.getElementsByTagName("script")[0];a.parentNode.insertBefore(r,a);for(var n=function(e){return function(){heap.push([e].concat(Array.prototype.slice.call(arguments,0)))}},p=["addEventProperties","addUserProperties","clearEventProperties","identify","resetIdentity","removeEventProperty","setEventProperties","track","unsetEventProperty"],o=0;o<p.length;o++)heap[p[o]]=n(p[o])};
-              heap.load("${process.env.NX_HEAP_TRACKING_ID}");
-            `}
-          </Script>
         </body>
       </Html>
     );
