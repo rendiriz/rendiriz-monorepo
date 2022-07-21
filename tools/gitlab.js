c8370155c68137527d89e7eaa161d9c1140e1f7f
Rendi Rizki <rendirizkir@gmail.com>
2022-07-22 00:34:20 +0700

    chore: keep calm and commit

--- tools/gitlab.js
+++ tools/gitlab.js
@@ -1,11 +1,12 @@
 const PROJECT_ID = 14510167;
 
-function getData() {
-  return fetch(
+async function getData() {
+  return await fetch(
     `https://gitlab.com/api/v4/projects/${PROJECT_ID}/repository/commits/dev`,
-  ).then((response) => response.json());
+  );
 }
 
-getData().then((data) => {
+getData().then(async (response) => {
+  const data = await response.json();
   console.log(data);
 });
