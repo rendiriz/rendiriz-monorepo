9597325161faf1a5e6aabd5861ecfb2c4e5d5e69
Rendi Rizki <rendirizkir@gmail.com>
2022-07-22 00:54:56 +0700

    chore: keep calm and commit

--- tools/gitlab.js
+++ tools/gitlab.js
@@ -1,8 +1,16 @@
+const { argv } = require('yargs');
 const fetch = (...args) =>
   import('node-fetch').then(({ default: fetch }) => fetch(...args));
 const PROJECT_ID = 14510167;
 
 async function getData() {
+  const { lref, lname, rref, rname } = argv;
+
+  console.log(lref);
+  console.log(lname);
+  console.log(rref);
+  console.log(rname);
+
   return await fetch(
     `https://gitlab.com/api/v4/projects/${PROJECT_ID}/repository/commits/dev`,
   );
