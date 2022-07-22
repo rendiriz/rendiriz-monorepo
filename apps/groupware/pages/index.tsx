32c64fb0f3183ec1a0dae93770410d35f5ad8e1e
Rendi Rizki <rendirizkir@gmail.com>
2022-07-22 17:01:49 +0700

    fix(groupware): module

--- apps/groupware/pages/index.tsx
+++ apps/groupware/pages/index.tsx
@@ -2,7 +2,7 @@ import { Container } from '@rendiriz-ecosystem/groupware/components';
 import { trpc } from '../utils/trpc';
 
 export function HomePage() {
-  const { data } = trpc.useQuery(['example.hello']);
+  const { data } = trpc.useQuery(['example.hello', { text: 'client' }]);
 
   if (!data) {
     return <div>Loading...</div>;
--- apps/groupware/utils/trpc.ts
+++ apps/groupware/utils/trpc.ts
@@ -1,5 +1,5 @@
 // src/utils/trpc.ts
-import type { AppRouter } from '@rendiriz-ecosystem/groupware/server';
+import type { AppRouter } from '../server/router';
 import { createReactQueryHooks } from '@trpc/react';
 
 export const trpc = createReactQueryHooks<AppRouter>();
